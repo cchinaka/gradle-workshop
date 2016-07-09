6510ab5e883a0b57ecfadc226b9e69375e42f1d1
= <cchinaka@gmail.com>
2016-07-09 19:47:09 +0100

    updates

---
+++ WorkshopEntity/src/main/java/ng/com/workshop/model/mongo/copy/Workshop.java
@@ -0,0 +1,48 @@
+package ng.com.workshop.model.mongo.copy;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+
+@Document
+public class Workshop {
+
+    @Id
+    private String id;
+
+    @Field("workShopName")
+    private String name;
+
+    private String type;
+
+
+    public String getId() {
+        return id;
+    }
+
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public String getType() {
+        return type;
+    }
+
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
